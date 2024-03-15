1bee2e0f6b84676a2a522334f37e746ec49f2c18
davidz811 <davidb.coldea@gmail.com>
2024-03-15 20:26:09 +0200

    added calendar

---
+++ components/bookingTable/bookingTable.jsx
@@ -0,0 +1,14 @@
+const bookingTable = () => {
+
+    return (
+        <table>
+            <thead>
+                <th>Room</th>
+                <th>Day</th>
+                <th>Nr of Members</th>
+            </thead>
+        </table>
+    )
+}
+
+export default bookingTable
