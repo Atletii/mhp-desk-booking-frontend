f7badaf03806262e2b32e829e1b24d5e693fb66d
Oprișor Raul-Alexandru <oprisorraul@gmail.com>
2024-03-15 19:07:57 +0200

    fixed form issues

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,7 +1,9 @@
 'use client';
+import InputField from '@/components/form/InputField';
 import BackgroundImage from '../../components/common/BackgroundImage'
 import { database } from '@/firebase/firebaseConfig';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import RedirectButton from '@/components/common/RedirectButton';
 
 export default function LoginPage() {
   function handleSignIn(e) {
@@ -12,7 +14,6 @@ export default function LoginPage() {
     signInWithEmailAndPassword(database, email, password)
       .then(data => {
         console.log(data, "authData");
-        // Navigate and set logged in state if needed
       })
       .catch(error => {
         alert(error.code);
@@ -22,23 +23,16 @@ export default function LoginPage() {
 
   return (
     <BackgroundImage src={`./images/background-mobile.jpg`}>
-      <div className="p-10 bg-white rounded-lg shadow-xl">
-        <h1 className="text-2xl font-bold text-center mb-4">MHP Desk Booking</h1>
-        <form onSubmit={(e) => handleSignIn(e)}>
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-sm font-semibold text-gray-700 mb-2">E-Mail</label>
-            <input type="email" id="email" placeholder="joe.smith@mhp.com" className="w-full p-2 border border-gray-300 rounded-md" />
-          </div>
-          <div className="mb-6">
-            <label htmlFor="password" className="block text-sm font-semibold text-gray-700 mb-2">Password</label>
-            <input type="password" id="password" placeholder="Password" className="w-full p-2 border border-gray-300 rounded-md" />
-          </div>
-          <div className="mb-6">
-            <a href="#" className="text-sm text-blue-600 hover:underline">No Account? Register Here!</a>
-          </div>
-          <button type="submit" className="w-full p-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Login</button>
-        </form>
-      </div>
-    </BackgroundImage>
+    <div className="bg-white rounded-lg shadow-xl mx-4 bg-opacity-95">
+      <h1 className="p-5 text-blue-800 text-2xl font-extrabold text-center pb-2">MHP Desk Booking</h1>
+      <p className='mb-4'>Login</p>
+      <form className='p-10 pt-0'>
+        <InputField label="E-Mail" type="email" id="email" placeholder="mustermann@mhp.com" className="" />
+        <InputField label="Password" type="password" id="password" placeholder="Password" className="" />
+        <RedirectButton text="No Account? Register Here!" path='/register' />
+        <button type="submit" className="w-full p-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700">Login</button>
+      </form>
+    </div>
+  </BackgroundImage>
   );
 }
--- components/common/RedirectButton.jsx
+++ components/common/RedirectButton.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const RedirectButton = ({ text, path }) => {
   return (
     <Link href={path}>
-      <p className="inline-block text-blue-700 text-white font-bold py-2 px-4 rounded">
+      <p className="inline-block text-blue-700 font-bold py-2 px-4 rounded">
         {text}
       </p>
     </Link>
