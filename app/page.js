5a31752f2fe1463008d431c785da3023dd013df2
Oprișor Raul-Alexandru <oprisorraul@gmail.com>
2024-03-16 20:51:10 +0200

    commented crap code

--- app/page.js
+++ app/page.js
@@ -9,13 +9,13 @@ import { Calendar } from "@/components/ui/calendar";
 import React, { useState } from "react";
 
 export default function Home() {
-  sendRequestWithBearerToken("get", "/bookings/2024-01-01", null)
-    .then((response) => {
-      console.log("GET request successful:", response.data);
-    })
-    .catch((error) => {
-      console.error("Error in GET request:", error);
-    });
+  // sendRequestWithBearerToken("get", "/bookings/2024-01-01", null)
+  //   .then((response) => {
+  //     console.log("GET request successful:", response.data);
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error in GET request:", error);
+  //   });
 
   const [date, setDate] = useState(new Date());
   console.log(date);
@@ -42,12 +42,12 @@ export default function Home() {
           <div className="select-none">
             {/* Charts */}
             <ChartComp1
-              labels={['Monday', 'Thuesday', 'Wednesday', 'Thursday', 'Friday']}
+              labels={["Monday", "Thuesday", "Wednesday", "Thursday", "Friday"]}
               data={[10, 28, 59, 17, 90]}
               useClient={true}
             />
             <ChartComp1
-              labels={['9-11', '11-13', '13-15', '15-17']}
+              labels={["9-11", "11-13", "13-15", "15-17"]}
               data={[33, 60, 10, 5]}
               useClient={true}
             />
