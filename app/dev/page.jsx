1132b7ae550499aa3c4602e7cef65c466b864e88
Oprișor Raul-Alexandru <oprisorraul@gmail.com>
2024-03-16 16:39:46 +0200

    WIP: Map connect vector

--- app/dev/page.jsx
+++ app/dev/page.jsx
@@ -1,12 +1,9 @@
 "use client";
-import { Calendar } from "@/components/ui/calendar";
-import { useState } from "react";
+
 import Navbar from "@/components/navbar/Navbar";
 import SvgMap from "@/components/map/SvgMap";
 
 export default function Home() {
-  const [date, setDate] = useState(new Date());
-
   return (
     <>
       <Navbar />
--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import { DataTableDemo } from "@/components/data-table/RoomsDataTable";
+import SvgMap from "@/components/map/SvgMap";
 import { Calendar } from "@/components/ui/calendar";
 
 export default function Home() {
@@ -8,11 +9,13 @@ export default function Home() {
         <p>header</p>
       </header>
       <main>
-        <div className="flex p-2">
+        <div className="flex mx-4 mb-4 p-8 shadow-lg items-center justify-center rounded">
+          <SvgMap />
+        </div>
+        <div className="flex mx-4">
           <div className="w-2/3 p-4 shadow-lg">
             <DataTableDemo />
           </div>
-
           <div className="w-1/3 p-4 shadow-lg">
             <div className="flex flex-col">
               <Calendar />
